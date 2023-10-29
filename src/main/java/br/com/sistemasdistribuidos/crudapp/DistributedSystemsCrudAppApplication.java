4869f063e8c54dccba40e55bd09ca398c523ace3
yurifmorais <yfmorais@inf.ufsm.com>
2023-10-29 14:56:33 -0300

    first commit

---
+++ src/main/java/br/com/sistemasdistribuidos/crudapp/DistributedSystemsCrudAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.sistemasdistribuidos.crudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedSystemsCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedSystemsCrudAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sistemasdistribuidos/crudapp/DistributedSystemsCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sistemasdistribuidos.crudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedSystemsCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
